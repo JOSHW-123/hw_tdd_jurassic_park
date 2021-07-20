c4d2e7183be01f0801a21d79310079b0ddd40df1
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-20 21:09:33 +0100

    should have a name test ok

--- models/park.js
+++ models/park.js
@@ -1,10 +1,10 @@
 const Dinosaur = require("./dinosaur");
 
-const Park = function(name, ticket_price, collection_of_dinosaurs){
+const Park = function(name, ticket_price){
     this.name = name;
     this.ticket_price = ticket_price;
-    this.collection_of_dinosaurs = collection_of_dinosaurs;
+    this.collection_of_dinosaurs = [];
 }
 
 
-module.exports = Park
+module.exports = Park;
